80a5dbd62e7bcc6b41c9bab2edab7aeb1e4975e0
alexandre.mahdhaoui <alexandre.mahdhaoui@share-now.com>
2023-08-01 16:03:57 +0200

    fix(main): fixed gocache error in systemd

--- bare-metal-provisioning/bootstrap/build/install_runc.sh
+++ bare-metal-provisioning/bootstrap/build/install_runc.sh
@@ -6,6 +6,10 @@ set -xe
 WORKDIR="/tmp"
 cd "${WORKDIR}" || exit 1
 
+# Prepare GOCACHE
+export GOCACHE="/tmp/gocache"
+mkdir -p "${GOCACHE}"
+
 # Env
 REPO="https://github.com/opencontainers/runc"
 REPO_DIR="$(basename "${REPO}" .git)"
@@ -28,3 +32,4 @@ dnf remove -y protobuf-compiler golang
 # Cleanup
 cd "${WORKDIR}" || exit 1
 rm -rf "./${REPO_DIR}"
+rm -rf "${GOCACHE}"
