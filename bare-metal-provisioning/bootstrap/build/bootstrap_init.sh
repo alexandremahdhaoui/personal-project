b266c92df05148f2a89e4272e43f1dae020799d3
alexandre.mahdhaoui <alexandre.mahdhaoui@share-now.com>
2023-08-01 14:41:52 +0200

    fix(main): fixed bootstrapping

--- bare-metal-provisioning/bootstrap/build/bootstrap_init.sh
+++ bare-metal-provisioning/bootstrap/build/bootstrap_init.sh
@@ -4,6 +4,11 @@
 
 set -xe
 
+POD_CIDR="172.16.0.0/16"
+METALLB_POOL_PUBLIC="10.1.0.0-10.1.255.254"
+METALLB_POOL_RESTRICTED="10.0.0.2-10.0.0.4"
+METALCONF_IP="10.0.0.3"
+
 RELEASE="main"
 BASE_URL="https://raw.githubusercontent.com/alexandremahdhaoui/personal-project/${RELEASE}/bare-metal-provisioning/bootstrap/build"
 
@@ -12,7 +17,8 @@ URL="${BASE_URL}/node_init.sh"
 curl -sfL "${URL}" | sh -xe -
 
 # run kubeadm
-kubeadm reset -f && kubeadm init
+kubeadm reset -f
+kubeadm init --service-cidr="${POD_CIDR}"  --pod-network-cidr="${POD_CIDR}"
 
 # export kubeconfig
 export KUBECONFIG=/etc/kubernetes/admin.conf
@@ -21,17 +27,18 @@ echo "export KUBECONFIG=/etc/kubernetes/admin.conf" | tee -a /etc/bashrc
 # untaint bootstrap-init node
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 
+sleep 10
 # install a CNI network plugin
-curl -sfL "${BASE_URL}/install_network_flannel.sh" | sh -xse - "172.16.0.0/16"
-
+curl -sfL "${BASE_URL}/install_network_calico.sh" | sh -xse - "${POD_CIDR}"
+sleep 10
 # install Multus
 curl -sfL "${BASE_URL}/install_network_multus.sh" | sh -xe -
-
+sleep 10
+# install metallb
+curl -sfL "${BASE_URL}/install_metallb.sh" | sh -xse - "${METALLB_POOL_PUBLIC}" "${METALLB_POOL_RESTRICTED}"
+sleep 10
+# install metalconf
+curl -sfL "${BASE_URL}/install_metalconf.sh" | sh -xse - "${METALCONF_IP}"
+sleep 10
 # install kubevirt
 curl -sfL "${BASE_URL}/install_kubevirt.sh" | sh -xe -
-
-# install metallb
-curl -sfL "${BASE_URL}/install_metallb.sh" | sh -xe - "10.1.0.0-10.1.255.254" "10.0.0.2-10.0.0.4"
-
-# install metalconf
-curl -sfL "${BASE_URL}/install_metalconf.sh" | sh -xse - "10.0.0.3"
--- bare-metal-provisioning/bootstrap/build/install_network_flannel.sh
+++ bare-metal-provisioning/bootstrap/build/install_network_flannel.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-NETWORK_CIDR=${1:-"172.16.0.0/16"}
+POD_CIDR=${1:-"172.16.0.0/16"}
 
 URL="https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml"
-curl -sfL "${URL}" | sed "s@10.244.0.0/16@${NETWORK_CIDR}@" | kubectl apply -f -
+curl -sfL "${URL}" | sed "s@10.244.0.0/16@${POD_CIDR}@" | kubectl apply -f -
