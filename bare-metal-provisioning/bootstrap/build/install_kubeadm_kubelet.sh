606036689874565353cabfecb2c03f271e455766
alexandre.mahdhaoui <alexandre.mahdhaoui@share-now.com>
2023-08-01 13:34:18 +0200

    fix(main): updated install_kubeadm_kubelet.sh

--- bare-metal-provisioning/bootstrap/build/install_kubeadm_kubelet.sh
+++ bare-metal-provisioning/bootstrap/build/install_kubeadm_kubelet.sh
@@ -6,18 +6,17 @@ DOWNLOAD_DIR="/usr/local/bin"
 RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
 ARCH="amd64"
 
-cd "${DOWNLOAD_DIR}" || exit 1
-sudo curl -sL --remote-name-all "https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}"
-sudo chmod +x {kubeadm,kubelet}
+curl -sfL --remote-name-all "https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}" -o "${DOWNLOAD_DIR}"
+chmod 755 "${DOWNLOAD_DIR}/"{kubeadm,kubelet}
 
 RELEASE_VERSION="v0.15.1"
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubelet/lib/systemd/system/kubelet.service"\
   | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" \
-  | sudo tee /etc/systemd/system/kubelet.service
+  | tee /etc/systemd/system/kubelet.service
 
-sudo mkdir -p "/etc/systemd/system/kubelet.service.d"
+mkdir -p "/etc/systemd/system/kubelet.service.d"
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" \
   | sed "s:/usr/bin:${DOWNLOAD_DIR}:g" \
-  | sudo tee "/etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
+  | tee "/etc/systemd/system/kubelet.service.d/10-kubeadm.conf"
 
 systemctl enable --now kubelet
