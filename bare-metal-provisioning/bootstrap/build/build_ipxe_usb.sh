1946afb6e799799fc98ce8c17c869dee73cb5b48
alexandre.mahdhaoui <alexandre.mahdhaoui@share-now.com>
2023-08-03 01:09:24 +0200

    fix(main): added ipxe usb build

--- bare-metal-provisioning/bootstrap/build/build_ipxe_usb.sh
+++ bare-metal-provisioning/bootstrap/build/build_ipxe_usb.sh
@@ -1,18 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 set -xe
 
-# DISCLAIMER: This script is intended to run inside an init-container, therefore cleanup's are skipped
-# test: kubectl run fedora --image fedora:latest --command -- sleep 3600 && kubectl exec -it fedora -- bash
-
-# url to webserver hosting the iPXE script
 IPXE_CONFIG_URL="${1:-"10.0.0.3/ipxe/config"}"
 # OUTPUT_DIR is an emptyDir volume is mounted to this specified path
 OUTPUT_DIR="${2:-"/ipxe-efi"}"
 
 # Prerequisites
-dnf install -y git make gcc binutils perl xz mtools xz-devel
+dnf install -y git make gcc binutils perl xz xz-devel mtools
 
+rm -rf /tmp/workdir
 # prepare workdir
 WORKDIR="/tmp/workdir"
 mkdir "${WORKDIR}"
@@ -35,6 +32,6 @@ cd ipxe/src || exit 1
 sed -i 's/#undef  DOWNLOAD_PROTO_HTTPS/#define DOWNLOAD_PROTO_HTTPS/' ./config/general.h
 
 # compile iPXE image
-BIN=bin-x86_64-efi/ipxe.efi
+BIN=bin/ipxe.usb
 make "${BIN}" EMBED="${IPXE_SCRIPT}" NO_WERROR=1
 cp -f "${BIN}" "${OUTPUT_DIR}"
