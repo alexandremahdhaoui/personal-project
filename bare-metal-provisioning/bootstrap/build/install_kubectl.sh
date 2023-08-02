9f9c1ef6c33d6bacbd1fe0d699a5911905eb2894
alexandre.mahdhaoui <alexandre.mahdhaoui@share-now.com>
2023-08-02 11:35:34 +0200

    fix(main): added kubectl aliases

--- bare-metal-provisioning/bootstrap/build/install_kubectl.sh
+++ bare-metal-provisioning/bootstrap/build/install_kubectl.sh
@@ -6,3 +6,6 @@ RELEASE=$(curl -sL -s https://dl.k8s.io/release/stable.txt)
 
 curl -sLO "https://dl.k8s.io/release/${RELEASE}/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# install
+echo '. <(curl -sfL https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases)' | tee -a /etc/profile
